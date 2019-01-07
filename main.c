fb1fdd532a43ebb584cdc2f734f18c2512c91266
xcj <cijun2008@163.com>
2019-01-07 11:05:10 +0800

    ok

--- main.c
+++ main.c
@@ -3,7 +3,10 @@ int main(void)
 	int i;
 	int j;
 	printf("hello world\r\n");
-
+	printf("hello world\r\n");
+	printf("hello world\r\n");
+	printf("hello world\r\n");
+	printf("hello world\r\n");
 
 
 	return 0;
